52603409311f686f51f14a3512f78e742b15c8d3
Robert  Dinu <vjroby@gmail.com>
2016-03-14 07:29:00 +0200

    added profile page

--- src/main/java/masterSpringMvc/controller/TwitterController.java
+++ src/main/java/masterSpringMvc/controller/TwitterController.java
@@ -7,10 +7,12 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class TwitterController {
@@ -32,5 +34,17 @@ public class TwitterController {
         List<Tweet> tweets = searchResults.getTweets();
         model.addAttribute("tweets", tweets);
         return "resultPage";
+    }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+        String search = request.getParameter("search");
+        if(search.toLowerCase().contains("struts")) {
+            redirectAttributes.addFlashAttribute("error", "Try using spring instead!");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search", search);
+        return  "redirect:result";
+
     }
 }
