6be740616c4d6af4f1665c00848488c1aa76900e
Robert  Dinu <vjroby@gmail.com>
2016-04-13 07:31:43 +0300

    added redirect to ssl

--- src/main/java/masterSpringMvc/config/WebSecurityConfiguration.java
+++ src/main/java/masterSpringMvc/config/WebSecurityConfiguration.java
@@ -9,6 +9,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .requiresChannel().anyRequest().requiresSecure()
+                .and()
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/profile")
