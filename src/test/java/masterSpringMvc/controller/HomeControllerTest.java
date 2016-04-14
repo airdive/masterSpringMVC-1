3dd5359892c8be1bcefa3493aff0dab72535cf7e
Robert  Dinu <vjroby@gmail.com>
2016-04-14 07:12:17 +0300

    test user tastes and create SessionBuilder

--- src/test/java/masterSpringMvc/controller/HomeControllerTest.java
+++ src/test/java/masterSpringMvc/controller/HomeControllerTest.java
@@ -1,21 +1,23 @@
 package masterSpringMvc.controller;
 
 import masterSpringMvc.MasterSpringMvcApplication;
+import masterSpringMvc.SessionBuilder;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MasterSpringMvcApplication.
@@ -40,6 +42,15 @@ public class HomeControllerTest {
                 .andDo(print())
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/profile"));
+    }
+
+    @Test
+    public void should_redirect_to_tastes() throws Exception {
+        MockHttpSession session = new SessionBuilder().userTastes("spring", "groovy").build();
+
+        this.mockMvc.perform(get("/").session(session))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/search/mixed;keywords=spring,groovy"));
     }
 
 }
